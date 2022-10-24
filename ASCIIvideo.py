392fa0394d9e9a501deaa500bc11e3643e743928
Patryk Korycki <pat.korycki@gmail.com>
2022-10-25 00:43:26 +0200

    Comparing OpenCV and MoviePy. In conclusion speed of both performances is comparable.

--- ASCIIvideo.py
+++ ASCIIvideo.py
@@ -1,15 +1,25 @@
 import moviepy.editor as mp
 import numpy as np
+import cv2
 
 #########################
 ##### Video segment #####
 #########################
 
-def Import_Vid(path):   # Zaimportowanie pliku wideo
+def Import_Vid(path):   # Zaimportowanie pliku wideo z wykorzystaniem biblioteki MoviePy
     global film
     film = mp.VideoFileClip(path)
 
-def Extract_frame(num): # Pobranie klatki
+# def Import_Vid(path):   # Zaimportowanie pliku wideo z wykorzystaniem biblioteki OpenCV2
+#     global film
+#     film = cv2.VideoCapture(path)
+
+def Extract_frame(num): # Pobranie klatki z wykorzystaniem biblioteki MoviePy
     global film
     frame = film.get_frame(num).tolist()
     return frame
+
+# def Extract_frame(num): # Pobranie klatki z wykorzystaniem biblioteki OpenCV2
+#     global film
+#     ret, frame = film.read()
+#     return frame.tolist()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import ASCIIaudio
 import ASCIIvideo
 import os
 import sys
+import time
 
 def Import_Module():        # Ta funkcja jest potrzebna tylko podczas użytkowania ziezainstalowanego modułu
     sys.path.append("build/lib.linux-x86_64-cpython-310/") 
@@ -10,16 +11,21 @@ if __name__ == '__main__':
     Import_Module()
     import ASCIIart
 
-    ASCIIaudio.Soundtrack_extract("media_files/Al.mp4")
-    ASCIIaudio.Soundmodule_init()
-    ASCIIaudio.Do_some_music_things()
+    #ASCIIaudio.Soundtrack_extract("media_files/Al.mp4")
+    #ASCIIaudio.Soundmodule_init()
+    #ASCIIaudio.Do_some_music_things()
 
     ASCIIvideo.Import_Vid("media_files/Webdriver.mp4")
     f = ASCIIvideo.Extract_frame(0)
-
+    
     ASCIIart.def_frame(len(f[0]), len(f))
     ASCIIart.load_frame(f)
-
+    
+    start = time.time()
     # ... 
-
+    for i in range(30):
+        f = ASCIIvideo.Extract_frame(i)
+        ASCIIart.load_frame(f)
+    stop = time.time()
+    print("\n ", 30/(stop-start))
     ASCIIart.clean()
